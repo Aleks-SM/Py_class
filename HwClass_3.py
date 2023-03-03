ff08d71a47bc0a7b8338c44b84e5c1409d130eaf
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-03-03 16:09:36 +0400

    Update HwClass_3.py

--- HwClass_3.py
+++ HwClass_3.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
     
     def rate_hw(self, lector, course, grade):
-        if isinstance(lector, Lectors) and course in self.courses_attached:
+        if isinstance(lector, Lecturer) and course in self.courses_attached:
             if course in lector.grades:
                 lector.grades[course] += [grade]
             else:
@@ -22,15 +22,21 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
               
-class Lectors(Mentor, Student):
+class Lecturer(Mentor, Student):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
         self.grades = {}
+    
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.grades}'
  
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
         
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -45,12 +51,12 @@ best_student.courses_in_progress += ['Python']
  
 reviewer = Reviewer('Some', 'Buddy')
 reviewer.courses_attached += ['Python']
- 
+
 reviewer.rate_hw(best_student, 'Python', 10)
 reviewer.rate_hw(best_student, 'Python', 10)
 reviewer.rate_hw(best_student, 'Python', 10)
 
-lector = Lectors('Petra', 'Berger')
+lector = Lecturer('Oleg', 'Byligin')
 lector.courses_attached += ['Python']
 
 lector.rate_hw(lector, 'Python', 10)
@@ -61,3 +67,6 @@ print(best_student.grades)
 print(reviewer.name, reviewer.surname, reviewer.courses_attached)
 print(lector.name, lector.surname, lector.courses_attached)
 print(lector.grades)
+print(str(reviewer))
+print()
+print(str(lector))
