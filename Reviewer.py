a189e1d8c978e09249bc66a139da2b49c8493a69
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-03-03 13:22:33 +0400

    Update Reviewer.py

--- Reviewer.py
+++ Reviewer.py
@@ -1,9 +1,7 @@
 class Reviewer(Mentor):
     def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-   
+        Mentor.__init__(self, name, surname)
+        
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
