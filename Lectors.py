a4b980f0b5d82ee4c2900bc186567ad1f854315b
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-03-03 13:23:17 +0400

    Update Lectors.py

--- Lectors.py
+++ Lectors.py
@@ -2,12 +2,3 @@ class Lectors(Mentor):
     def __init__(self, name, surname):
         Mentor.__init__(self, name, surname)
         self.grades = {}
-        
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
