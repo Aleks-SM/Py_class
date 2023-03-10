70f8b39f88f3a1b94decce8fee56745beb0a82e2
AleksSM <aleks-m83@mail.ru>
2023-03-10 20:45:50 +0400

    add func compare student

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class Human:
     def __init__(self, name, surname):
         self.atributes = {'name': name, 'surname': surname}
 
+
 class Student(Human):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -41,6 +42,7 @@ class Student(Human):
         else:
             return 'Ошибка'
 
+
 class Mentor(Human):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -59,7 +61,6 @@ class Lecturer(Mentor):
         res = round(sum(lst) / len(lst), 2)
         return res
 
-
     def __str__(self):
         return f'Имя: {self.atributes.get("name")}\n' \
                f'Фамилия: {self.atributes.get("surname")}\n' \
@@ -73,6 +74,7 @@ class Lecturer(Mentor):
 
     def __le__(self, other):
         return self.average_rate() <= other.average_rate()
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -93,7 +95,7 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-#расчет средней оценки по заданному курсу
+# расчет средней оценки по заданному курсу
 def avg_grade_in_course(lst_student, course):
     for student in lst_student:
         if course in student.courses_in_progress:
@@ -113,6 +115,7 @@ def compare_students(student_1, student_2):
               f'студента {student_2.atributes.get("name")} {student_2.atributes.get("surname")},'
         return res
 
+
 def compare_lecturer(lecturer_1, lecturer_2):
     if isinstance(lecturer_1, Lecturer) and isinstance(lecturer_2, Lecturer):
         res = f'Средняя оценка за лекции {lecturer_1.average_rate()} ' \
@@ -120,6 +123,7 @@ def compare_lecturer(lecturer_1, lecturer_2):
               f'лучше чем средняя оценка {lecturer_2.average_rate()} у ' \
               f'лектора {lecturer_2.atributes.get("name")} {lecturer_2.atributes.get("surname")},'
         return res
+
 
 lecturer_1 = Lecturer('Petra', 'Berger')
 lecturer_1.courses_attached += ['Python']
@@ -154,10 +158,10 @@ student_2.rate_hw(lecturer_2, 'Git', 10)
 student_2.rate_hw(lecturer_2, 'Git', 10)
 student_2.rate_hw(lecturer_2, 'Git', 10)
 
-reviewer_1.rate_hw(student_1 , 'Python', 10)
-reviewer_1.rate_hw(student_1 , 'Python', 9)
-reviewer_1.rate_hw(student_1 , 'Python', 9)
-reviewer_1.rate_hw(student_1 , 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Python', 10)
 
 reviewer_1.rate_hw(student_2, 'Python', 3)
 reviewer_1.rate_hw(student_2, 'Python', 4)
