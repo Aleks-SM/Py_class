8e1475b4812f7f48236397f167f2d04710645d1a
AleksSM <aleks-m83@mail.ru>
2023-03-10 20:18:10 +0400

    add func compare student

--- main.py
+++ main.py
@@ -51,15 +51,16 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-    def __str__(self):
-        return f'Имя: {self.atributes.get("name")}\n' \
-               f'Фамилия: {self.atributes.get("surname")}\n' \
-               f'Средняя оценка за лекции: {self.average_rate()}\n'
     def average_rate(self):
         lst = []
         for value in self.grades.values():
             lst.extend(value)
         return '{:.3}'.format(sum(lst) / len(lst))
+
+    def __str__(self):
+        return f'Имя: {self.atributes.get("name")}\n' \
+               f'Фамилия: {self.atributes.get("surname")}\n' \
+               f'Средняя оценка за лекции: {self.average_rate()}\n'
 
     def __eq__(self, other):  # ==
         return average_rate(self) == average_rate(other)
@@ -89,56 +90,61 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-def average_rate(person):
-    lst = []
-    for value in person.grades.values():
-        lst.extend(value)
-    return '{:.3}'.format(sum(lst) / len(lst))
+#расчет средней оценки по заданному курсу
+def avg_grade_in_course(lst_student, course):
+    for student in lst_student:
+        if course in student.courses_in_progress:
+            average_grades = round(sum(student.grades.get(course)) / len(student.grades.get(course)), 1)
+            res = f'Средняя оценка по курсу {course} студента ' \
+                  f'{student.atributes.get("name")} ' \
+                  f'{student.atributes.get("surname")} равна {average_grades}\n'
+            print(res)
+    return
 
-def avg_grade_in_course(students, course):
-    all_grades_in_course = []
-    if len(self.grades > 0:
-          for value in self.grades.value():
-              all_grades_courses.extend(value)
-          if len(self.grades > 0:
-              average_grades = round(sum(all_grades_courses) / len(all_gades_courses, 1)
-              return average_grades
-    return 'No grades'
 
 def compare_students(student_1, student_2):
     if isinstance(student_1, Student) and isinstance(student_2, Student):
         res = f'Средняя оценка {student_1.average_rate()} ' \
               f'студента {student_1.atributes.get("name")} {student_1.atributes.get("surname")} ' \
-              f'лучше чем оценка {student_2.average_rate()} у ' \
+              f'лучше чем средняя оценка {student_2.average_rate()} у ' \
               f'студента {student_2.atributes.get("name")} {student_2.atributes.get("surname")},'
+        return res
+
+def compare_lecturer(lecturer_1, lecturer_2):
+    if isinstance(lecturer_1, Lecturer) and isinstance(lecturer_2, Lecturer):
+        res = f'Средняя оценка за лекции {lecturer_1.average_rate()} ' \
+              f'лектора {lecturer_1.atributes.get("name")} {lecturer_1.atributes.get("surname")} ' \
+              f'лучше чем средняя оценка {lecturer_2.average_rate()} у ' \
+              f'лектора {lecturer_2.atributes.get("name")} {lecturer_2.atributes.get("surname")},'
         return res
 
 lecturer_1 = Lecturer('Petra', 'Berger')
 lecturer_1.courses_attached += ['Python']
 
-lecturer_2= Lecturer('Lisa', 'Middelhauve')
+lecturer_2 = Lecturer('Lisa', 'Middelhauve')
 lecturer_2.courses_attached += ['Git']
 
 reviewer_1 = Reviewer('Annet', 'Olsen')
 reviewer_1.courses_attached += ['Python']
+reviewer_1.courses_attached += ['Git']
 
 reviewer_2 = Reviewer('Manuela', 'Kraller')
 reviewer_2.courses_attached += ['Java']
 
 student_1 = Student('Ruoy', 'Eman')
-student_1 .finished_courses += ['Введение в программирование']
-student_1 .courses_in_progress += ['Python']
-student_1 .courses_in_progress += ['Git']
+student_1.finished_courses += ['Введение в программирование']
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
 
 student_2 = Student('Olyx', 'Ivanov')
 student_2.finished_courses += ['Введение в программирование']
 student_2.courses_in_progress += ['Python']
-student_2.courses_in_progress += ['Java']
+student_2.courses_in_progress += ['Git']
 
-student_1 .rate_hw(lecturer_1, 'Python', 10)
-student_1 .rate_hw(lecturer_1, 'Python', 10)
-student_1 .rate_hw(lecturer_1, 'Python', 9)
-student_1 .rate_hw(lecturer_1, 'Python', 10)
+student_1.rate_hw(lecturer_1, 'Python', 10)
+student_1.rate_hw(lecturer_1, 'Python', 10)
+student_1.rate_hw(lecturer_1, 'Python', 9)
+student_1.rate_hw(lecturer_1, 'Python', 10)
 
 student_2.rate_hw(lecturer_2, 'Git', 10)
 student_2.rate_hw(lecturer_2, 'Git', 10)
@@ -154,11 +160,18 @@ reviewer_1.rate_hw(student_2, 'Python', 3)
 reviewer_1.rate_hw(student_2, 'Python', 4)
 reviewer_1.rate_hw(student_2, 'Python', 4)
 reviewer_1.rate_hw(student_2, 'Python', 5)
+reviewer_1.rate_hw(student_2, 'Python', 3)
 
-students = [student_1, student_2]
+reviewer_1.rate_hw(student_2, 'Git', 4)
+reviewer_1.rate_hw(student_2, 'Git', 4)
+reviewer_1.rate_hw(student_2, 'Git', 4)
+reviewer_1.rate_hw(student_2, 'Git', 5)
+
+students = [student_2, student_1]
+avg_grade_in_course(students, 'Python')
 
 print(compare_students(student_1, student_2))
-
+print(compare_lecturer(lecturer_1, lecturer_2))
 print(str(student_1))
 print(str(student_2))
 
